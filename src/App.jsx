8951a2f5a4184990896a8ca101e4a75ed952a80d
pwangy <blipsandclicks@gmail.com>
2025-07-16 18:07:22 -0700

    prefer jsx version of popout link svg, file clean up

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <Navbar />
       <Hero />
       <About />
-      <Experience />
+      {/* <Experience /> */}
       <Projects />
       <Contact />
     </div>
--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,21 +1,21 @@
 import { getImageUrl } from '../utils'
 import github from '../assets/icons/githubicon.png'
-// import link from '../assets/icons/link.png'
+import { LinkPopout } from '../assets/icons/LinkPopout'
 
-const LiveLink = ({ className, ...props }) => (
-        <svg 
-        width="12" 
-        height="12" 
-        viewBox="0 0 24 24" 
-        fill="none" 
-        className={className}
-        {...props}
-        >
-        <path d="M18 13V19C18 19.5304 17.7893 20.0391 17.4142 20.4142C17.0391 20.7893 16.5304 21 16 21H5C4.46957 21 3.96086 20.7893 3.58579 20.4142C3.21071 20.0391 3 19.5304 3 19V8C3 7.46957 3.21071 6.96086 3.58579 6.58579C3.96086 6.21071 4.46957 6 5 6H11" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-        <path d="M15 3H21V9" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-        <path d="M10 14L21 3" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-    </svg>
-)
+// const LiveLink = ({ className, ...props }) => (
+//         <svg 
+//         width="12" 
+//         height="12" 
+//         viewBox="0 0 24 24" 
+//         fill="none" 
+//         className={className}
+//         {...props}
+//         >
+//         <path d="M18 13V19C18 19.5304 17.7893 20.0391 17.4142 20.4142C17.0391 20.7893 16.5304 21 16 21H5C4.46957 21 3.96086 20.7893 3.58579 20.4142C3.21071 20.0391 3 19.5304 3 19V8C3 7.46957 3.21071 6.96086 3.58579 6.58579C3.96086 6.21071 4.46957 6 5 6H11" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+//         <path d="M15 3H21V9" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+//         <path d="M10 14L21 3" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+//     </svg>
+// )
 
 export const ProjectCard = ({
 	project: { title, imageSrc, description, skills, demo, source }
@@ -38,7 +38,7 @@ export const ProjectCard = ({
 			{/* demo link */}
 			<a className='card-header' href={demo} target='_blank' rel='noopener noreferrer'>
 				<h3>{title}</h3>
-				<LiveLink className='live-link-icon' aria-hidden='true' />
+				<LinkPopout className='live-link-icon' aria-hidden='true' />
 			</a>
 			<p className='card-description card-spacer'>{description}</p>
 			{/* github link */}
